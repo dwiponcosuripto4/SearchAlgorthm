4f8e07eedab914cbe1276f2fbf916b4d48cbf9b9
Ilham  Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2022-10-26 16:51:55 +0700

    add enter elements

--- SearchAlgorthm/Program.cs
+++ SearchAlgorthm/Program.cs
@@ -39,8 +39,68 @@ namespace SearchAlgorthm
                 arr[i] = Int32.Parse(s1);
             }
         }
-        static void Main(string[] args)
+        public void BinarySearch()
         {
+            char ch;
+            do
+            {
+                // accept the number to be searched
+                Console.Write
+                    ("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                // Apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the index of the middle elements
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\nNumber of comparison :" + ctr);
+
+                Console.Write("\nContinue search (y/n):");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
+        }
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                //Accept the number to be searched
+                Console.WriteLine("\nEnter the element you want to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine
+                            ("\n" + item.ToString() + " found st position " + (i + 1).ToString());
+                        break;
+                    }
+                }
+            }
         }
     }
 }
